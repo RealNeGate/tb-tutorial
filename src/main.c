b46e90cfbb7a08a6189eabaf3667ae845da59d75
NeGate <yasserarguelles2506@gmail.com>
2022-10-06 20:44:08 -0400

    Added basic variables

--- src/main.c
+++ src/main.c
@@ -14,6 +14,49 @@ static void foo(int param) {
 ////////////////////////////////
 // Parser
 ////////////////////////////////
+typedef struct Type Type;
+typedef struct Param Param;
+
+// it's just easier to do a linked list ok
+struct Param {
+    Param* next;
+    char* name;
+
+    // due to our type inference we don't specify the type of a parameter
+    // on creation and instead just do it by matching against the first
+    // invocation
+    Type* type;
+};
+
+struct Type {
+    enum {
+        TYPE_HOLE, // unresolved at the moment
+        TYPE_INT,  // 32bit integer
+        TYPE_MAP,  // just a table
+        TYPE_FUNC, // expression with runtime inputs
+    } tag;
+    union {
+        struct {
+            Type* key;
+            Type* val;
+        } map;
+        struct {
+            Type* ret;
+            size_t param_count;
+            Param* params;
+        } func;
+    };
+};
+
+typedef struct Sym {
+    char* name;
+    TB_Reg value;
+} Sym;
+
+enum { MAX_SYMBOLS = 1024 };
+int sym_count;
+Sym syms[MAX_SYMBOLS];
+
 #define CTABLE_LEN(a) (sizeof(a) / sizeof(a[0]))
 #define CTABLE_GET(a, i) (i < CTABLE_LEN(a) ? a[i] : 0)
 
@@ -24,6 +67,20 @@ static int preds[] = {
 
 static TB_Reg unary(TB_Function* func, NL_Lexer* restrict l) {
     switch (l->token_type) {
+        case NL_TOKEN_IDENT: {
+            size_t len = l->token_end - l->token_start;
+            const char* str = l->token_start;
+            NL_READ_TOKEN(l);
+
+            for (size_t i = sym_count; i--;) {
+                if (strncmp(str, syms[i].name, len) == 0) {
+                    return tb_inst_load(func, TB_TYPE_I32, syms[i].value, 4);
+                }
+            }
+
+            fprintf(stderr, "unknown variable '%.*s'\n", (int) len, str);
+            abort();
+        }
         case NL_TOKEN_NUMBER: {
             unsigned long long n = nl_parse_int(l);
             NL_READ_TOKEN(l);
@@ -57,7 +114,23 @@ static TB_Reg binop(TB_Function* func, NL_Lexer* restrict l, int min_prec) {
 }
 
 static TB_Reg expr(TB_Function* func, NL_Lexer* restrict l) {
-    if (NL_TRY_EAT_STRING(l, "if")) {
+    if (NL_TRY_EAT_STRING(l, "var")) {
+        // 'var' ident '=' expr
+        char* name = nl_token_as_cstr(l);
+        NL_READ_TOKEN(l);
+
+        if (!NL_TRY_EAT(l, '=')) {
+            abort();
+        }
+
+        TB_Reg storage = tb_inst_local(func, 4, 4);
+        TB_Reg initial = expr(func, l);
+        tb_inst_store(func, TB_TYPE_I32, storage, initial, 4);
+
+        assert(sym_count < MAX_SYMBOLS);
+        syms[sym_count++] = (Sym){ .name = name, .value = storage };
+        return initial;
+    } else if (NL_TRY_EAT_STRING(l, "if")) {
         TB_Label true_lbl = tb_basic_block_create(func);
         TB_Label false_lbl = tb_basic_block_create(func);
 
@@ -72,24 +145,22 @@ static TB_Reg expr(TB_Function* func, NL_Lexer* restrict l) {
         TB_Reg hit = expr(func, l);
         TB_Label hit_lbl = tb_inst_get_label(func);
 
-        TB_Reg miss = TB_NULL_REG;
-        TB_Label miss_lbl = 0;
-
         if (NL_TRY_EAT_STRING(l, "else")) {
             TB_Label skip_lbl = tb_basic_block_create(func);
             tb_inst_goto(func, skip_lbl);
             tb_inst_set_label(func, false_lbl);
 
-            miss = expr(func, l);
-            miss_lbl = tb_inst_get_label(func);
+            TB_Reg miss = expr(func, l);
+            TB_Label miss_lbl = tb_inst_get_label(func);
 
             tb_inst_goto(func, skip_lbl);
             tb_inst_set_label(func, skip_lbl);
+            return tb_inst_phi2(func, hit_lbl, hit, miss_lbl, miss);
         } else {
+            tb_inst_goto(func, false_lbl);
             tb_inst_set_label(func, false_lbl);
+            return TB_NULL_REG;
         }
-
-        return tb_inst_phi2(func, hit_lbl, hit, miss_lbl, miss);
     } else if (l->token_type == '(') {
         NL_READ_TOKEN(l);
 
@@ -130,7 +201,7 @@ int main(void) {
         func = tb_function_create(mod, "entry", TB_LINKAGE_PUBLIC);
         tb_function_set_prototype(func, proto);
 
-        TB_Reg result = parse(func, "(16 + 4*4, if 16 + 16 then 6 else 0)");
+        TB_Reg result = parse(func, "(var apple = 16 + 4*4, if 16 + 16 then apple else 0)");
         tb_inst_vcall(func, TB_TYPE_VOID, tb_inst_param(func, 0), 1, &result);
         tb_inst_ret(func, TB_NULL_REG);
 
